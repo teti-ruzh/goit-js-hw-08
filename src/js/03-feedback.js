01e8c599d3745954e62aef8060fd2c676e89cbf6
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2023-02-19 21:35:47 +0200

    Did Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[type="email"]');
+const message = document.querySelector('textarea[name="message"]');
+
+const formData = {}
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+populateFormData();
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.removeItem(STORAGE_KEY);
+    };
+
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+};
+
+function populateFormData() {
+const savedFormData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+if(savedFormData) {
+    email.value = savedFormData.email;
+    message.value = savedFormData.message;
+}
+}
+
+
 
