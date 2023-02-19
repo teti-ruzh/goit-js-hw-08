018e889188b4d60d9622052671e22a0e4c41fd18
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2023-02-19 19:00:04 +0200

    Did Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
+
+function createGalleryMarkup(images) {
+return images.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}).join('')
+};
+
+
+  const lightbox = new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
