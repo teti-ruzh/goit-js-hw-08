9fa3b7292a9447b299e66065e9d36a86b3eb892f
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2023-02-20 13:05:43 +0200

    Did Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onSaveCurrentTime, 1000));
+
+function onSaveCurrentTime({duration, seconds}) {
+localStorage.setItem(STORAGE_KEY, seconds);
+if(duration === seconds) {
+    localStorage.removeItem(STORAGE_KEY);
+}
+};
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0.0).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
